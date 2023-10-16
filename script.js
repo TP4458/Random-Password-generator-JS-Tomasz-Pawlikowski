bffec6f2678914fd20c685feea3471dd0dd31a67
Tomasz Pawlikowski <tp4458@gmail.com>
2023-10-16 23:57:28 +0100

    added 3 working prompts for charcters. fixed the generator

--- script.js
+++ script.js
@@ -2,36 +2,45 @@
 //var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-const characters ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+const characters ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 const numbers = "0123456789";
-const specialChar = "!#$%&'()*+,-./:;<=>?@][[^_`{|}~";
-var tempString = "";
-var passLength = 55;    //
+const specialChar = "\\!#$%&'()*+,-./:;<=>?@][[^_`{|}~" + '"'
+var tempArray = "";      //this will concatenate ther strings to use in the IF
+var passLength = 128;    //prompt will be here
+var randomPass = "";
+
 var password = function(length) {
-    let result =' ';
-    const passLength = characters.length; //change to tempstring
-    for (let i = 0;i < length; i++) {
-      result += tempString.charAt(Math.floor(Math.random() * passLength));
+    var tempArrayLength = tempArray.length;                                           //converts array length to a number
+    for ( i = 0; i < length; i++) {
+      randomPass += tempArray.charAt(Math.floor(Math.random() * tempArrayLength));    //charAt takes a random character from tempArray
     }
-    return result;
+   // console.log(passLength);
+    console.log(tempArray.length);
+    console.log(randomPass.length)
+    return randomPass;
+
 }
-console.log(password(11));  //change to passLength
+console.log(password(234));
+
+  //change to passLength
 
 let passType = prompt("Select 1 to use letters only, 2 to use letters and numbers, 3 to use letters, numers, and special characters");
   if (passType == 1) {
-    tempString = characters;
-    console.log(password(111));
+    tempArray = characters;
+    console.log(password(passLength));
+    
   }
-  else if (passType == 2) {
-    tempString = characters + numbers;
-    console.log(password(111));
+    else if (passType == 2) {
+    tempArray = characters + numbers;
+    console.log(password(passLength));
   }
-  else if (passType == 3) {
-    tempString = characters + numbers + specialChar;
-    console.log(password(111));
+    else if (passType == 3) {
+    tempArray = characters + numbers + specialChar;
+    console.log(password(passLength));
   }
     else {
       console.log("ERROR ERROR ERROR")
+      alert("++?????++ Out of Cheese Error. Redo From Start.")
   }
   
 // var passwordText = document.querySelector("#password");
