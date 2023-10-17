359f9a52e56b7d74433dd645b395992c332accb1
Tomasz Pawlikowski <tp4458@gmail.com>
2023-10-17 16:56:55 +0100

    changed the way character prompts work.added password length prompt

--- script.js
+++ script.js
@@ -2,46 +2,47 @@
 //var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-const characters ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+const charactersUp ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const charactersLo ="abcdefghijklmnopqrstuvwxyz";
 const numbers = "0123456789";
 const specialChar = "\\!#$%&'()*+,-./:;<=>?@][[^_`{|}~" + '"'
-var tempArray = "";      //this will concatenate ther strings to use in the IF
-var passLength = 128;    //prompt will be here
-var randomPass = "";
+var tempArray = "";                                                                   //this will contain characters used in password generation
+var randomPass = "";   
+var passLength ="";
 
-var password = function(length) {
-    var tempArrayLength = tempArray.length;                                           //converts array length to a number
-    for ( i = 0; i < length; i++) {
-      randomPass += tempArray.charAt(Math.floor(Math.random() * tempArrayLength));    //charAt takes a random character from tempArray
-    }
-   // console.log(passLength);
-    console.log(tempArray.length);
-    console.log(randomPass.length)
-    return randomPass;
+do{
+ passLength = prompt("select password length between 8 and 128 characters.");         //prompt will repeat until correct input
+ } while(passLength < 8 || passLength > 128);
 
+var charLoConfirm = confirm("Would you like to use lower case letters?")
+ if (charLoConfirm) {
+  tempArray = charactersLo;
+ }
+
+var charHiConfirm = confirm("Would you like to use upper case letters?")
+ if (charHiConfirm) {
+  tempArray = tempArray + charactersUp;
+
+ }
+var numberConfirm = confirm("Would you like to use numbers?")
+ if (numberConfirm) {
+  tempArray = tempArray + numbers;
+}  
+
+var specialConfirm = confirm("Would you like to use special charcters?")
+if (specialConfirm) {
+  tempArray = tempArray + specialChar;
 }
-console.log(password(234));
 
-  //change to passLength
-
-let passType = prompt("Select 1 to use letters only, 2 to use letters and numbers, 3 to use letters, numers, and special characters");
-  if (passType == 1) {
-    tempArray = characters;
-    console.log(password(passLength));
-    
+//
+var password = function(length) {
+  var tempArrayLength = tempArray.length;                                           //converts array length to a number
+  for ( i = 0; i < length; i++) {
+    randomPass += tempArray.charAt(Math.floor(Math.random() * tempArrayLength));    //charAt takes a random character from tempArray
   }
-    else if (passType == 2) {
-    tempArray = characters + numbers;
-    console.log(password(passLength));
-  }
-    else if (passType == 3) {
-    tempArray = characters + numbers + specialChar;
-    console.log(password(passLength));
-  }
-    else {
-      console.log("ERROR ERROR ERROR")
-      alert("++?????++ Out of Cheese Error. Redo From Start.")
-  }
+  return randomPass;
+}
+console.log(password(passLength))
   
 // var passwordText = document.querySelector("#password");
 
