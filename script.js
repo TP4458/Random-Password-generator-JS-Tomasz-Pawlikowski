541e93bb393635b58c22d7671ef2b50869b63766
Tomasz Pawlikowski <tp4458@gmail.com>
2023-10-18 00:30:29 +0100

    working button and text output

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Assignment Code
-//var generateBtn = document.querySelector("#generate");
+generateBtn = document.querySelector("#generate");
+//generateBtn.addEventListener("click", password);
+
 
 // Write password to the #password input
 const charactersUp ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -9,46 +11,45 @@ const specialChar = "\\!#$%&'()*+,-./:;<=>?@][[^_`{|}~" + '"'
 var tempArray = "";                                                                   //this will contain characters used in password generation
 var randomPass = "";   
 var passLength ="";
+var passBox = document.querySelector("#password");
 
-do{
- passLength = prompt("select password length between 8 and 128 characters.");         //prompt will repeat until correct input
- } while(passLength < 8 || passLength > 128);
+var writePassword = function(){
+  passbox = "";
+  do{
+    passLength = prompt("select password length between 8 and 128 characters.");         //prompt will repeat until correct input
+    } while(passLength < 8 || passLength > 128);
+  
+    var charLoConfirm = confirm("Would you like to use lower case letters?")
+    if (charLoConfirm) {
+      tempArray = charactersLo;
+    }
+  
+    var charHiConfirm = confirm("Would you like to use upper case letters?")
+    if (charHiConfirm) {
+      tempArray = tempArray + charactersUp;
+  
+    }
+    var numberConfirm = confirm("Would you like to use numbers?")
+    if (numberConfirm) {
+      tempArray = tempArray + numbers;
+    }  
+  
+    var specialConfirm = confirm("Would you like to use special charcters?")
+    if (specialConfirm) {
+      tempArray = tempArray + specialChar;
+    };
 
-var charLoConfirm = confirm("Would you like to use lower case letters?")
- if (charLoConfirm) {
-  tempArray = charactersLo;
- }
-
-var charHiConfirm = confirm("Would you like to use upper case letters?")
- if (charHiConfirm) {
-  tempArray = tempArray + charactersUp;
-
- }
-var numberConfirm = confirm("Would you like to use numbers?")
- if (numberConfirm) {
-  tempArray = tempArray + numbers;
-}  
-
-var specialConfirm = confirm("Would you like to use special charcters?")
-if (specialConfirm) {
-  tempArray = tempArray + specialChar;
-}
-
-//
 var password = function(length) {
   var tempArrayLength = tempArray.length;                                           //converts array length to a number
   for ( i = 0; i < length; i++) {
     randomPass += tempArray.charAt(Math.floor(Math.random() * tempArrayLength));    //charAt takes a random character from tempArray
   }
-  return randomPass;
+    return randomPass;
 }
-console.log(password(passLength))
-  
-// var passwordText = document.querySelector("#password");
+passBox.textContent = password(passLength)
+}
+// function clearbox(){
+//   document.querySelector("#password").value = "";
 
-//  passwordText.value = password;
-
-
-
-// Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
+// }
+generateBtn.addEventListener("click", writePassword);
