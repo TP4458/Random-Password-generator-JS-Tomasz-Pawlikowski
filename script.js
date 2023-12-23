ad8612e93570bcb65598b42f97289a1f79719a2e
Tomasz Pawlikowski <tp4458@gmail.com>
2023-12-23 23:38:17 +0000

    fixed a bug causing old password to persist

--- script.js
+++ script.js
@@ -1,43 +1,39 @@
 // Assignment Code
 generateBtn = document.querySelector("#generate");
-//generateBtn.addEventListener("click", password);
 
-
-// Write password to the #password input
 const charactersUp ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const charactersLo ="abcdefghijklmnopqrstuvwxyz";
 const numbers = "0123456789";
 const specialChar = "\\!#$%&'()*+,-./:;<=>?@][[^_`{|}~" + '"'
-var tempArray = "";                                                                   //this will contain characters used in password generation
-var randomPass = "";   
-var passLength ="";
-var passBox = document.querySelector("#password");
 
-var writePassword = function(){
-  passbox = "";
+
+
+let writePassword = function(){
+  let tempArray = "";                                                                   
+  let passLength ="";
+  let passBox = document.querySelector("#password");
+  // character number prompt will repeat until it receives correct input
   do{
-    passLength = prompt("select password length between 8 and 128 characters.");         //prompt will repeat until correct input
+    passLength = prompt("select password length between 8 and 128 characters.");         
     } while(passLength < 8 || passLength > 128);
-  
-    var charLoConfirm = confirm("Would you like to use lower case letters?")
+
+  // charcter prompts will concatenate strings to tempArray
+  var charLoConfirm = confirm("Would you like to use lower case letters?")
     if (charLoConfirm) {
       tempArray = charactersLo;
-    }
-  
-    var charHiConfirm = confirm("Would you like to use upper case letters?")
+  }
+  var charHiConfirm = confirm("Would you like to use upper case letters?")
     if (charHiConfirm) {
       tempArray = tempArray + charactersUp;
-  
-    }
-    var numberConfirm = confirm("Would you like to use numbers?")
+  }
+  var numberConfirm = confirm("Would you like to use numbers?")
     if (numberConfirm) {
       tempArray = tempArray + numbers;
-    }  
-  
-    var specialConfirm = confirm("Would you like to use special charcters?")
+  }  
+  var specialConfirm = confirm("Would you like to use special charcters?")
     if (specialConfirm) {
       tempArray = tempArray + specialChar;
-    };
+  };
 
 var password = function(length) {
   var tempArrayLength = tempArray.length;                                           //converts array length to a number
@@ -48,8 +44,4 @@ var password = function(length) {
 }
 passBox.textContent = password(passLength)
 }
-// function clearbox(){
-//   document.querySelector("#password").value = "";
-
-// }
 generateBtn.addEventListener("click", writePassword);
